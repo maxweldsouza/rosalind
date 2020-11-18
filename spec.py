f2e52aed5e408495c1c1a3d7cd434c0a5b4d40b5
Maxwel Dsouza <maxweldsouza.in@gmail.com>
2020-11-18 23:26:14 +0530

    spec

---
+++ spec.py
@@ -0,0 +1,32 @@
+import util
+import operator
+import collections
+
+def spec(l):
+    table = [(p, w) for p, w in util.monoisotopic_mass_table.items()]
+
+    s = sorted(l)
+    ans = []
+    for i in range(len(s)-1):
+        d = s[i+1] - s[i]
+        m, _ = min([(p, abs(w-d)) for p, w in table], key=operator.itemgetter(1))
+        ans.append(m)
+
+    return ''.join(ans)
+
+assert spec([
+3524.8542,
+3710.9335,
+3841.974,
+3970.0326,
+4057.0646
+]) == 'WMQS'
+
+def main():
+    with open('rosalind_spec.txt') as f:
+        arr = f.readlines()
+        arr = [float(x.strip()) for x in arr]
+        print (spec(arr))
+
+
+main()
